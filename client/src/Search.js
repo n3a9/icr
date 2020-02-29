70ff30f4eeeb191fb2f69c126c812edb289552c5
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-02-28 20:20:14 -0600

    Add search functionality

---
+++ client/src/Search.js
@@ -0,0 +1,51 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { AutoComplete } from "antd";
+import courseJSON from "./data/courses.json";
+
+const { Option } = AutoComplete;
+
+const Search = () => {
+  const [courses, setCourses] = useState([]);
+
+  const parseCourses = useCallback(() => {
+    let courseNames = new Set();
+
+    courseJSON.map(course =>
+      courseNames.add(`${course.Subject} ${course.Number}`)
+    );
+
+    setCourses(
+      Array.from(courseNames).map(course => (
+        <Option key={course} value={course}>
+          {course}
+        </Option>
+      ))
+    );
+  }, [setCourses]);
+
+  const filter = useCallback(
+    (input, option) =>
+      option.props.children
+        .toUpperCase()
+        .substring(0, input.length)
+        .indexOf(input.toUpperCase()) !== -1 ||
+      option.props.children.toUpperCase().indexOf(input.toUpperCase()) !== -1,
+    []
+  );
+
+  const onSearchSelect = useCallback(console.log("Selected!"));
+
+  useEffect(parseCourses, []);
+
+  return (
+    <AutoComplete
+      className="search"
+      dataSource={courses}
+      placeholder="Search for a Course"
+      filterOption={filter}
+      onSelect={onSearchSelect}
+    />
+  );
+};
+
+export default Search;
