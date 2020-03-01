5015494aba651ab27162e59219a0244c435eded8
alansf2 <alansf2@illinois.edu>
2020-02-29 23:05:33 -0600

    added grade

--- api/src/models/review.js
+++ api/src/models/review.js
@@ -4,8 +4,9 @@ const Review = new mongoose.Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
   rating: { type: Number, required: true },
-  difficulty: {type: Number, required: true},
-  instructor: { type: String, required: true}, 
+  difficulty: {type: Number, required: true },
+  instructor: { type: String, required: true },
+  grade: { type: String }
 });
 
 module.exports = mongoose.model("Review", Review);
