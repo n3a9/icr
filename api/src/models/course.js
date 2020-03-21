fceb75c1b1f349b1e3c52fb6e7125ac752fb7f72
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-03-20 17:48:13 -0700

    Add difficulty to course model

--- api/src/models/course.js
+++ api/src/models/course.js
@@ -13,6 +13,7 @@ const Course = new mongoose.Schema({
   title: { type: String, required: true },
   reviews: { type: [Review], default: [] },
   rating: { type: Number, required: true }
+  difficulty: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Course", Course);
